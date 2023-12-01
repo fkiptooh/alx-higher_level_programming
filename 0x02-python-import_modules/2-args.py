cccfd1d75456f6f439222c9a6275ddc7691793c8
fkiptooh <fkiptooh@gmail.com>
2023-12-01 12:52:25 +0300

    code refactor

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-from sys import argv
-def main():
+if __name__ == "__main__":
+    from sys import argv
+
     num_args = len(argv) - 1
     print(f"{num_args}{'.' if num_args == 0 else ':'} argument{'s' if num_args != 1 else ''}")
 
     if num_args > 0:
         for i, arg in enumerate(argv[1:], start=1):
             print(f"{i}: {arg}")
-if __name__ == "__main__":
-    main()
