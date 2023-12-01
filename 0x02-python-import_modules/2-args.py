5c41404cb54c11175a71077c2ae5fb15de650b73
fkiptooh <fkiptooh@gmail.com>
2023-12-01 13:32:07 +0300

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Printing number of and list of arguments."""
-    from sys import argv
+    """Print the number of and list of arguments."""
+    import sys
 
-    num_args = len(argv) - 1
-    if num_args == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(num_args))
-    for i in range(num_args):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
